931621b17a2eef1dc41ce507125b0569c1c1f3d8
Tammy <tammy@Tammy.local>
2015-08-10 00:10:27 +0800

    change language

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
     // page is now ready, initialize the calendar...
 
     $('#calendar').fullCalendar({
+    	lang: 'zh-tw'
         // put your options and callbacks here
-    })
+    });
 
 });
