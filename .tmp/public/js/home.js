77ab1f9cac4408ffe0e9a704a1a03d3054ee5dcf
Tammy <tammy@Tammy.local>
2015-08-11 23:59:20 +0800

    self-practice

--- .tmp/public/js/home.js
+++ .tmp/public/js/home.js
@@ -6,5 +6,30 @@ $(document).ready(function() {
     	lang: 'zh-tw'
         // put your options and callbacks here
     });
-
+});
+$(document).ready(function() {
+    $('#calendar').fullCalendar({
+    	defaultDate: '2015-08-11',
+    	businessHours: true,
+		editable: true,
+	    events: [
+	        {
+	            title  : '吳小姐',
+	            start  : '2015-08-09',
+	            constraint: 'businessHours' 
+	        },
+	        {
+	            title  : '蔡先生',
+	            start  : '2015-08-05',
+	            end    : '2015-08-07',
+	            constraint: 'businessHours'
+	        },
+	        {
+	            title  : '陳先生',
+	            start  : '2015-08-11',//T12:30:00',
+	            constraint: 'businessHours'
+	            //allDay : false // will make the time show
+	        }
+	    ]
+	});
 });
